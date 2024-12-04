91d9ae738ee22188d5e8769facf82a1a9ecbeca5
Maxim Surin <surin.maks@yandex.ru>
2024-12-04 15:53:02 +0300

    Все тесты

--- homework_lesson16/test/array_tests.cpp
+++ homework_lesson16/test/array_tests.cpp
@@ -2,17 +2,111 @@
 #include "../include/MyArray.hpp"
 
 TEST(Array, Create) {
-    // Arrange
     MyArray<int> myArray;
 
-    // Act (empty for this test)
-
-    // Assert
     ASSERT_EQ(myArray.size(), 0);
 }
 
+TEST(Array, push_back) {
 
-// int main(int argc, char** argv) {
-//     testing::InitGoogleTest(&argc, argv);
-//     return RUN_ALL_TESTS();
-// }
+    MyArray<int> myArray;
+
+    myArray.insertFromEnd(1);
+    std::string strRez = "1 ";
+    std::string strGetList = myArray.getList();
+
+    ASSERT_EQ(myArray.size(), 1);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(Array, insertFromBegin) {
+
+    MyArray<int> myArray;
+
+    myArray.insertFromBegin(1);
+    std::string strRez = "1 ";
+    std::string strGetList = myArray.getList();
+    
+    ASSERT_EQ(myArray.size(), 1);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(Array, insertFromPosition) {
+
+    MyArray<int> myArray;
+
+    myArray.insertFromEnd(1);
+    myArray.insertFromEnd(2);
+    myArray.insertFromEnd(3);
+    myArray.insertFromPosition(10,2);
+    std::string strRez = "1 10 2 3 ";
+    std::string strGetList = myArray.getList();
+
+    ASSERT_EQ(myArray.size(), 4);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(Array, eraseFromEnd) {
+
+    MyArray<int> myArray;
+
+    myArray.insertFromEnd(1);
+    myArray.insertFromEnd(2);
+    myArray.eraseFromEnd();
+    std::string strRez = "1 ";
+    std::string strGetList = myArray.getList();
+    ASSERT_EQ(myArray.size(), 1);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(Array, eraseFromBegin) {
+
+    MyArray<int> myArray;
+
+    myArray.insertFromEnd(1);
+    myArray.insertFromEnd(2);
+    myArray.eraseFromBegin();
+    std::string strRez = "2 ";
+    std::string strGetList = myArray.getList();
+
+    ASSERT_EQ(myArray.size(), 1);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(Array, eraseFromPosition) {
+
+    MyArray<int> myArray;
+    myArray.insertFromEnd(1);
+    myArray.insertFromEnd(2);
+    myArray.insertFromEnd(3);
+
+    myArray.eraseFromPosition(2);
+    std::string strRez = "1 3 ";
+    std::string strGetList = myArray.getList();
+
+    ASSERT_EQ(myArray.size(), 2);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(Array, getList) {
+
+    MyArray<int> myArray;
+    myArray.insertFromEnd(1);
+    myArray.insertFromEnd(2);
+
+    std::string strRez = "1 2 ";
+    std::string strGetList = myArray.getList();
+
+    ASSERT_EQ(myArray.size(), 2);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(Array, size) {
+
+    MyArray<int> myArray;
+    
+    myArray.insertFromEnd(1);
+    myArray.insertFromEnd(2);
+
+    ASSERT_EQ(myArray.size(), 2);
+}
--- homework_lesson16/test/list_tests.cpp
+++ homework_lesson16/test/list_tests.cpp
@@ -2,17 +2,108 @@
 #include "../include/MyList.hpp"
 
 TEST(List, Create) {
-    // Arrange
+
     MyList myList0{0};
-
-    // Act (empty for this test)
-
-    // Assert
+    
     ASSERT_EQ(myList0.size(), 1);
 }
 
+TEST(List, push_back) {
+    
+    MyList myList0{0};
+    
+    myList0.insertFromEnd(1);
+    std::string strRez = "0 1 ";
+    std::string strGetList = myList0.getList();
+    
+    ASSERT_EQ(myList0.size(), 2);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
 
-// int main(int argc, char** argv) {
-//     testing::InitGoogleTest(&argc, argv);
-//     return RUN_ALL_TESTS();
-// }
+TEST(List, insertFromBegin) {
+    
+    MyList myList0{0};
+    
+    myList0.insertFromBegin(1);
+    std::string strRez = "1 0 ";
+    std::string strGetList = myList0.getList();
+    
+    ASSERT_EQ(myList0.size(), 2);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(List, insertFromPosition) {
+    
+    MyList myList0{0};
+    myList0.insertFromEnd(1);
+    myList0.insertFromEnd(2);
+    
+    myList0.insertFromPosition(10,2);
+    std::string strRez = "0 10 1 2 ";
+    std::string strGetList = myList0.getList();
+    
+    ASSERT_EQ(myList0.size(), 4);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(List, eraseFromEnd) {
+    
+    MyList myList0{0};
+    myList0.insertFromEnd(1);
+    
+    myList0.eraseFromEnd();
+    std::string strRez = "0 ";
+    std::string strGetList = myList0.getList();
+    
+    ASSERT_EQ(myList0.size(), 1);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(List, eraseFromBegin) {
+    
+    MyList myList0{0};
+    myList0.insertFromEnd(1);
+    
+    myList0.eraseFromBegin();
+    std::string strRez = "1 ";
+    std::string strGetList = myList0.getList();
+    
+    ASSERT_EQ(myList0.size(), 1);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(List, eraseFromPosition) {
+    
+    MyList myList0{0};
+    myList0.insertFromEnd(1);
+    myList0.insertFromEnd(2);
+    
+    myList0.eraseFromPosition(2);
+    std::string strRez = "0 2 ";
+    std::string strGetList = myList0.getList();
+    
+    ASSERT_EQ(myList0.size(), 2);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(List, getList) {
+    
+    MyList myList0{0};
+    myList0.insertFromEnd(1);
+    myList0.insertFromEnd(2);
+    
+    std::string strRez = "0 1 2 ";
+    std::string strGetList = myList0.getList();
+    
+    ASSERT_EQ(myList0.size(), 3);
+    ASSERT_STREQ(strGetList.c_str() ,strRez.c_str());
+}
+
+TEST(List, size) {
+    
+    MyList myList0{0};
+    myList0.insertFromEnd(1);
+    myList0.insertFromEnd(2);
+    
+    ASSERT_EQ(myList0.size(), 3);
+}
