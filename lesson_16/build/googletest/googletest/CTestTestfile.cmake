7ed279e413c7f491e8a1f4004cf49be1f9880209
Maxim Surin <surin.maks@yandex.ru>
2024-12-18 11:49:12 +0300

    исходники новой лекции

--- lesson_16/build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/surinmm/Projects/otusCppBasic/lesson_16/project_with_gtest/googletest/googletest
-# Build directory: /home/surinmm/Projects/otusCppBasic/lesson_16/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
