f0f6a164da173e1cb3999bf49e4de408eb4401d9
Maxim Surin <surin.maks@yandex.ru>
2024-12-18 11:56:16 +0300

    src lesson 23

---
+++ lesson_23/01_header_only/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+int main() {
+
+    const json doc = {
+        {"pi", 3.141},
+        {"happy", true},
+        {"name", "Niels"},
+        {"nothing", nullptr},
+        {
+            "answer", {
+                {"everything", 42}
+            }
+        },
+        {"list", {1, 0, 2}},
+        {
+            "object", {
+                {"currency", "USD"},
+                {"value", 42.99}
+            }
+        }
+    };
+
+    std::cout << doc.dump(4) << '\n';
+}
---
+++ lesson_23/02_linking/main.cpp
@@ -0,0 +1,6 @@
+
+#include <gtest/gtest.h>
+
+TEST(Simple, alone) {
+    ASSERT_EQ(4, 2 * 2);
+}
